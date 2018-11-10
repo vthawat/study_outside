b4ab9de7025f71a37e1cde5c1dba5f3ef75a27cd
vthawat <thawat.va@email.psu.ac.th>
2018-11-10 16:22:07 +0700

    update schedule

--- application/modules/staff/views/schedule.php
+++ application/modules/staff/views/schedule.php
@@ -4,8 +4,8 @@
     <h3 class="thai-font text-info"><i class="fa fa-fw fa-table"></i>กำหนดการเดินทางจากระบบสร้างอัตโนมัติ</h3>
     <table class="table table-hover">
         <thead class="bg-gray">
-            <th>เวลา</th>
-            <th>กิจกรรม/สถานที่</th>
+            <th class="col-md-2 col-xs-3 text-center">เวลา</th>
+            <th>สถานที่</th>
         </thead>
         <tbody>
         <?php $routing=json_decode($trips->routing)?>
@@ -17,10 +17,18 @@
                  $start_time=$this->study_trip->schedule_time_shift($end_time);
                  $end_time=$this->study_trip->schedule_time_shift($start_time,$rout->duration);
                  
-      
+                // show place location
+                $start_place=$this->study_place->get_by_id($rout->start_place_id);
+                if($rout->start_place_id==0)
+                    $start_location_details='อ.หาดใหญ่ จ.สงขลา';
+                else $start_location_details='อ.'.$start_place->AMPHUR_NAME.' จ.'.$start_place->PROVINCE_NAME;
+                $end_place=$this->study_place->get_by_id($rout->end_place_id);
+                if($rout->end_place_id==0)
+                    $end_location_details='อ.หาดใหญ่ จ.สงขลา';
+                else $end_location_details='อ.'.$end_place->AMPHUR_NAME.' จ.'.$end_place->PROVINCE_NAME;
             ?>
-            <td><?=$start_time?> - <?=$end_time?></td>
-            <td><?=$rout->start_location?><i class="fa fa-fw fa-angle-double-right"></i><?=$rout->end_location?></td>
+            <td class="text-center"><?=$start_time?> - <?=$end_time?></td>
+            <td><?=$rout->start_location?> <?=$start_location_details?> <i class="fa fa-fw fa-angle-double-right"></i><?=$rout->end_location?> <?=$end_location_details?></td>
         </tr>
         <?php endif;?>
         <?php endforeach?>
