2513b7ad7e01d5e4e7a6de547e01c8474a1b1de2
unknown <thawat.v@gmail.com>
2019-04-28 17:13:34 +0700

    update

--- application/modules/staff/views/student.php
+++ application/modules/staff/views/student.php
@@ -8,7 +8,10 @@
 	<tbody>
 		<?php
 		$rows = [];
+		$r=0;
 		foreach ($sheetData->getRowIterator() AS $row) {
+			if($r!=0)
+			{
 			print '<tr>';
 			$cellIterator = $row->getCellIterator();
 			$cellIterator->setIterateOnlyExistingCells(FALSE); // This loops through all cells,
@@ -22,6 +25,8 @@
 			//$rows[] = $cells;
 			//print_r($rows);
 			print '</tr>';
+		}
+			$r++;
 		}
 		?>
 	</tbody>
