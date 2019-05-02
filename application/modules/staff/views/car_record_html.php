6198caac330292867a1c5cf95ac595a239a1d050
vthawat <thawat.va@email.psu.ac.th>
2019-05-03 03:58:46 +0700

    update

--- application/modules/staff/views/car_record_html.php
+++ application/modules/staff/views/car_record_html.php
@@ -48,5 +48,10 @@
 <p class="sec-1"><b>เรียน</b>&nbsp;{booking_reference}</p>
 <p class="sec-2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ด้วย{booking_objective}&nbsp;มีความประสงค์จะเดินทางไปปฏิบัติงานที่{booking_go_place}&nbsp;โดยมีวัตถุประสงค์เพื่อ (กรณีเดินทางเพื่อปฏิบัติงานในโครงการวิจัยให้ระบุ
 ชื่อโครงการวิจัยด้วย)&nbsp;{booking_research_name}&nbsp;จึงใคร่ขออนุมัติใช้รถและเดินทางไปปฏิบัติงาน/
-ปฏิบัติงานนอกเวลา เพื่อปฏิบัติงานดังกล่าว โดยให้รถรับ&nbsp;{booking_depart_place}&nbsp;ในวันที่&nbsp;{booking_depart_date}&nbsp;เวลา&nbsp;{booking_depart_time}</p>
+ปฏิบัติงานนอกเวลา เพื่อปฏิบัติงานดังกล่าว โดยให้รถรับ&nbsp;{booking_depart_place}&nbsp;ในวันที่&nbsp;{booking_depart_date}&nbsp;เวลา&nbsp;{booking_depart_time}&nbsp;น.&nbsp;กลับถึงคณะฯ&nbsp;ในวันที่&nbsp;{booking_arrive_date}&nbsp;เวลา&nbsp;{booking_depart_time}&nbsp;น.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ไม่ขอเบิกค่าใช้จ่าย&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ขอเบิกค่าใช้จ่ายต่าง ๆ ดังนี้<br>
+<u>หมวดค่าใช้สอย</u>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u>หมวดค่าวัสดุและค่าตอบแทน</u><br>
+1.&nbsp;ค่าวัสดุน้ำมันเชื้อเพลิง&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{item_money_type1_1}&nbsp;บาท&nbsp;&nbsp;&nbsp;&nbsp;1.&nbsp;ค่าวัสดุน้ำมันเชื้อเพลิง&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{item_money_type1_1}&nbsp;บาท<br>
+2.&nbsp;ค่าเบี้ยเลี้ยง{item_money_type1_2_duration}&nbsp;บาท&nbsp;&nbsp;&nbsp;&nbsp;2.&nbsp;ค่าล่วงเวลา&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{item_money_type2_2}&nbsp;บาท
+</p>
 </body>
